ea46f325e7e9fbf5467c6f1d1537f48c466056f6
Florian Walter <fw@florian-walter.ch>
2026-06-02 11:41:24 +0200

    add simple flake configuration

---
+++ flake.nix
@@ -0,0 +1,110 @@
+{
+  description = "devtools CLI";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      formatter = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        pkgs.nixpkgs-fmt
+      );
+
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.buildGo126Module {
+            pname = "devtools";
+            version = "0.1.0";
+
+            src = self;
+            vendorHash = null;
+
+            subPackages = [ "cmd/devtools" ];
+
+            nativeCheckInputs = [ pkgs.git ];
+          };
+        }
+      );
+
+      apps = forAllSystems (
+        system:
+        let
+          pkg = self.packages.${system}.default;
+        in
+        {
+          default = {
+            type = "app";
+            program = "${pkg}/bin/devtools";
+            meta.description = "Run the devtools CLI";
+          };
+        }
+      );
+
+      checks = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = self.packages.${system}.default;
+
+          go-test = pkgs.buildGo126Module {
+            pname = "devtools-tests";
+            version = "0.1.0";
+
+            src = self;
+            vendorHash = null;
+
+            doCheck = true;
+            nativeCheckInputs = [
+              pkgs.git
+              pkgs.tmux
+            ];
+
+            installPhase = ''
+              mkdir -p $out
+              touch $out/passed
+            '';
+          };
+        }
+      );
+
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.go_1_26
+              pkgs.gopls
+              pkgs.gotools
+              pkgs.git
+              pkgs.tmux
+            ];
+          };
+        }
+      );
+    };
+}
